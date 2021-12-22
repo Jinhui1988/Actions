c6501d2e31d76943f4f306b82a02d530f8c66ea7
jinhui <642440250@qq.com>
2021-12-22 16:30:40 +0800

    python actions test

---
+++ aaa.py
@@ -0,0 +1,29 @@
+
+john=0
+print(john)
+
+this="张金辉........fdfdfdfdfdfdfdfd辅导辅导辅导辅导辅导辅导辅导辅导辅导辅导辅导辅导辅导辅导辅导"
+print(this)
+
+name = input("what is your name?  ")
+print("hello," + name)
+
+name = "zhangjinhui"
+name2 = name.upper()
+print()
+
+
+nums = 164
+print(nums)
+
+
+
+
+
+
+
+
+
+
+
+
